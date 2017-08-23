4122a1173604c6c73d30ce63945b5759cdf4315b
Biro123 <biro123@ntlworld.com>
2017-08-23 19:23:08 +0100

    LT02 - background music, STATIC and SINGLETONS..  (ie single instance of classes.)

--- Assets/Scripts/MusicPlayer.cs
+++ Assets/Scripts/MusicPlayer.cs
@@ -4,10 +4,28 @@ using UnityEngine;
 
 public class MusicPlayer : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-        GameObject.DontDestroyOnLoad(gameObject);
-	}
+    static MusicPlayer instance = null;
+
+    private void Awake()
+    {
+        /// Singleton Pattern - used to ensure only one instance of this class
+        /// ever exists. This way we won't have the same tune playing over itself.
+        if (instance != null)
+        {
+            Destroy(gameObject);
+            print("MusicPlayer destroyed");
+        }
+        else
+        {
+            instance = this;
+            GameObject.DontDestroyOnLoad(gameObject);
+        }
+    }
+
+    // Use this for initialization
+    void Start () {
+        Debug.Log(" MusicPlayer Start: " + GetInstanceID());
+     }
 	
 	// Update is called once per frame
 	void Update () {
